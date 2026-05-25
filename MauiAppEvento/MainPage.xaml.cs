f011bc76b947f9018f94c2218498ebcd17d5d669
igor.batista <igor_batista97@hotmail.com>
2026-05-25 14:00:11 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppEvento/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MauiAppEvento.Models;
+
+namespace MauiAppEvento;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object? sender, EventArgs e)
+    {
+        try
+        {
+            Evento e1 = new Evento
+            {
+                NomeEvento = txt_nome.Text,
+                DataInicio = dt_inicio.Date.Value,
+                DataFim = dt_fim.Date.Value,
+                NumeroParticipantes = Convert.ToInt32(stp_participantes.Value),
+                LocalEvento = txt_local.Text,
+                CustoPorParticipante = Convert.ToDouble(txt_custo.Text)
+            };
+
+            await DisplayAlertAsync(
+                "Evento Cadastrado",
+                $"Evento: {e1.NomeEvento}\n" +
+                $"Duração: {e1.DuracaoEvento} dias\n" +
+                $"Custo Total: R$ {e1.CustoTotal}",
+                "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlertAsync("Erro", ex.Message, "OK");
+        }
+    }
+}
---
+++ MauiAppEvento/Models/Evento.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiAppEvento.Models
+{
+    public class Evento
+    {
+        public string? NomeEvento { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set; }
+
+        public int NumeroParticipantes { get; set; }
+
+        public string? LocalEvento { get; set; }
+
+        public double CustoPorParticipante { get; set; }
+
+        public int DuracaoEvento
+        {
+            get => DataFim.Subtract(DataInicio).Days;
+        }
+
+        public double CustoTotal
+        {
+            get => NumeroParticipantes * CustoPorParticipante;
+        }
+    }
+}
